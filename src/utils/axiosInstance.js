372def2004e02bd5608e083991a2783dd939cbb4
ashish7079 <ashishjha25032004@gmail.com>
2026-09-04 04:20:51 +0530

    fix production api url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+// const axiosInstance = axios.create({
+
+//   baseURL: "http://localhost:8080",
+
+// });
+
 const axiosInstance = axios.create({
 
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
 
 });
 
